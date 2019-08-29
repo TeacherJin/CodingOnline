a9a3c32f15f4000bb213dd4f3287995c27862eb3
jinzhenqian <jzq526@21cn.com>
2019-08-29 15:58:19 +0800

    完成了登录验证，并添加了在线编程页面（还没有实质性内容）

--- src/main/java/me/vk/CodingOnline/controller/StudentController.java
+++ src/main/java/me/vk/CodingOnline/controller/StudentController.java
@@ -3,12 +3,9 @@ package me.vk.CodingOnline.controller;
 import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
 import com.jfinal.ext.interceptor.SessionInViewInterceptor;
+import me.vk.CodingOnline.model.Student;
 import me.vk.CodingOnline.model.Verse;
 
-import javax.servlet.http.Cookie;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -24,12 +21,33 @@ public class StudentController extends Controller {
         int i=random.nextInt(verses.size());
         //将该随机数对应的诗词警句放到session当中;
         setSessionAttr("verse",verses.get(i).getStr("verse"));
-
+        //渲染页面，首页
         render("/index.html");
     }
 
     //处理登录过程
     public void login(){
+        //获取登录页面index.html传递过来的学号和密码
+        String studentId=getPara("StudentId");
+        String studentPassword=getPara("StudentPassword");
+        //从数据库学生信息表中获取相关的数据
+        Student student=Student.dao.findById(studentId);
+        //判断是否为合法用户
+        if(student !=null && student.getStr("password").equals(studentPassword) ){
+            //将学号和姓名写入session
+            setSessionAttr("StudentId",studentId);
+            setSessionAttr("StudentName",student.getStr("name"));
+            //跳转到editor.html页面
+            redirect("/editor");
+        }else{
+            //给出错误提示信息并提供跳转回首页的链接
+            renderHtml("输入的用户名或密码错误，请返回<a href=\"/index\">首页</a>并重新输入正确信息");
+        }
 
+    }
+
+    //渲染在线编程页面editor.html
+    public void editor(){
+        render("/editor.html");
     }
 }
