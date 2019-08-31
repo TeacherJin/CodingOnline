a351538f8a7201519d1de630311b92dc13f17be6
jinzhenqian <jzq526@21cn.com>
2019-08-31 22:48:11 +0800

    添加了提交代码、编译、运行、打开指定程序和退出登录的功能

--- src/main/java/me/vk/CodingOnline/controller/StudentController.java
+++ src/main/java/me/vk/CodingOnline/controller/StudentController.java
@@ -5,8 +5,9 @@ import com.jfinal.core.Controller;
 import com.jfinal.ext.interceptor.SessionInViewInterceptor;
 import me.vk.CodingOnline.model.Student;
 import me.vk.CodingOnline.model.Verse;
+import me.vk.CodingOnline.tools.Tools;
 
-import java.io.File;
+import java.io.*;
 import java.util.List;
 import java.util.Random;
 
@@ -49,6 +50,14 @@ public class StudentController extends Controller {
 
     //渲染在线编程页面editor.html
     public void editor() {
+        //获取学号，因为不是从前台传过来的，而是通过session传递的，所以使用getSessionAttr方法
+        String studentId=getSessionAttr("studentId");
+        //获取代码保存路径
+        String codesPath = getRequest().getServletContext().getRealPath("/codes") + "/" + studentId;
+        //获取Tools对象
+        Tools tool=new Tools(codesPath);
+        List<String> javaFiles=tool.listCodeFiles();
+        setSessionAttr("javaFiles",javaFiles);
         render("/editor.html");
     }
 
@@ -81,5 +90,76 @@ public class StudentController extends Controller {
                 renderHtml("<font color='red'>服务器出现问题，请联系管理员</font>");
             }
         }
+    }
+
+    //编译并运行程序
+    public void runCode(){
+        //获取前台页面的参数值
+        String className=getPara("className");
+        String code=getPara("code");
+        String studentId=getPara("studentId");
+        //获取用户名
+        Student student = Student.dao.findById(studentId);
+        String studentName=student.getStr("name");
+        //获取代码保存路径
+        String codesPath = getRequest().getServletContext().getRealPath("/codes") + "/" + studentId;
+        //获取Tools对象
+        Tools tool=new Tools(codesPath,className);
+        //保存代码文件
+        tool.writeCode(code);
+        //编译并运行程序，将结果保存起来
+        String result=tool.runCode();
+        //将结果存入session，为了方便显示，将之前获取的参数也存入session
+        setSessionAttr("result",result);
+        setSessionAttr("className",className);
+        setSessionAttr("code",code);
+        setSessionAttr("studentId",studentId);
+        setSessionAttr("studentName",studentName);
+        //跳转到editor页面
+        redirect("/editor");
+    }
+
+    //打开链接中指定的文件
+    public void openFile() throws FileNotFoundException {
+        //获取各类参数
+        String file=getPara("file");
+        String className=file.substring(0,file.lastIndexOf(".java"));
+        String studentId=getSessionAttr("studentId");
+        String studentName=new Student().dao.findById(studentId).getStr("name");
+        //获取代码保存路径
+        String codesPath = getRequest().getServletContext().getRealPath("/codes") + "/" + studentId;
+        String filePath=codesPath+"/"+file;
+        //读取文件中的内容
+        File f=new File(filePath);
+        String code="";
+        try  {
+            FileReader reader = new FileReader(f);
+            BufferedReader br=new BufferedReader(reader);
+            StringBuilder sb=new StringBuilder();
+            String s="";
+            while ((s=br.readLine())!=null){
+                sb.append(s+"\n");
+            }
+            reader.close();
+            br.close();
+            code=sb.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //把相关内容加入到session中
+        setSessionAttr("className",className);
+        setSessionAttr("code",code);
+        setSessionAttr("studentId",studentId);
+        setSessionAttr("studentName",studentName);
+        //跳转到editor页面
+        redirect("/editor");
+    }
+    //退出登录状态
+    public void logout(){
+        //清除所有的session
+        getSession().invalidate();
+        redirect("/index");
     }
 }
--- src/main/java/me/vk/CodingOnline/tools/Tools.java
+++ src/main/java/me/vk/CodingOnline/tools/Tools.java
@@ -1,34 +1,112 @@
 package me.vk.CodingOnline.tools;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Tools {
     //代码保存的路径
-    String codeDir;
+    private String codesPath;
     //代码保存的名称，不含后缀名
-    String className;
+    private String className;
+
     //构造方法
-    public Tools(String codeDir, String className) {
-        this.codeDir = codeDir;
+
+    public Tools(String codesPath) {
+        this.codesPath = codesPath;
+    }
+
+    public Tools(String codesPath, String className) {
+        this.codesPath = codesPath;
         this.className = className;
     }
+
     //错误提示信息
-    final String ERROR="服务器出现故障，请联系管理员";
-    public String runCode(){
+    final private String ERROR = "服务器出现故障，请联系管理员";
+
+    public String runCode() {
         //获取操作系统类型
-        String osType=System.getProperty("os.name");
+        String osType = System.getProperty("os.name");
         //程序运行结果
-        String result=ERROR;
+        String result = ERROR;
         //根据操作系统执行合适的方法，这里仅仅支持linux和windows
-        if(osType.toLowerCase().endsWith("linux")){
-            result=runCodeWithLinux();
+        if (osType.toLowerCase().endsWith("linux")) {
+            //编译和运行命令
+            String complie = "javac " + codesPath + "/" + className + ".java";
+            String run = "java -classpath " + codesPath + " " + className;
+            result = runCodeWithLinux(complie);
+            if (result.length() == 0) {
+                return runCodeWithLinux(run);
+            } else {
+                return result;
+            }
         }
         return result;
     }
 
-    private String runCodeWithLinux() {
+    //运行Linux的方法，cmd为要执行的命令
+    private String runCodeWithLinux(String cmd) {
         //程序运行结果
-        String result=ERROR;
-
-
+        String result = ERROR;
+        //生成Runtime对象
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            //生成调用complie命令的进程
+            Process processComplie = runtime.exec(cmd);
+            //获取运行结果，需要获取运行结果和错误信息
+            InputStream in = processComplie.getInputStream();
+            InputStream error = processComplie.getErrorStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(error));
+            String s = "";
+            StringBuilder sbResult = new StringBuilder();
+            StringBuilder sbError = new StringBuilder();
+            while ((s = reader.readLine()) != null) {
+                sbResult.append(s).append("\n");
+            }
+            while ((s = errorReader.readLine()) != null) {
+                sbError.append(s).append("\n");
+            }
+            //生成运行结果
+            result = sbResult.toString() + sbError.toString();
+            //关闭各种资源，结束进程
+            reader.close();
+            errorReader.close();
+            error.close();
+            in.close();
+            processComplie.destroy();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return result;
+    }
+    public void writeCode(String code){
+        FileSystem fileSystem= FileSystems.getDefault();
+        Path path=fileSystem.getPath(codesPath+"/"+className+".java");
+        try (BufferedWriter bw = Files.newBufferedWriter(path, Charset.forName("UTF-8"))) {
+            bw.write(code, 0, code.length());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //列出指定目录下所有以java为后缀名的文件
+    public List<String> listCodeFiles(){
+        System.out.println(codesPath);
+        File f=new File(codesPath);
+        File[] files=f.listFiles();
+        Arrays.sort(files);
+        List<String> javaFiles=new ArrayList<String>();
+        for(File file:files){
+            if(file.getName().toLowerCase().endsWith("java")){
+                javaFiles.add(file.getName());
+            }
+        }
+        return javaFiles;
     }
 }
